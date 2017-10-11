6b7676112950d0f89292d480c0a9868093ee7a40
Gabriel Duquette <gabrielduquette@gmail.com>
2017-10-11 19:20:01 -0400

    Add months view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,6 +20,9 @@ class ArticlesController < ApplicationController
     @articles = Article.all.order('created_at DESC').all
 
     @article = Article.new
+
+    @archive = Article.all.order("created_at DESC").
+      group_by { |article| article.created_at.strftime("%Y") }
   end
 
   def show
