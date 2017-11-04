4f32a72a8584cb70add05d04a9c892bd57d8a980
Gabriel Duquette <gabrielduquette@gmail.com>
2017-11-04 03:48:45 -0400

    Fix login and redirect to current page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,14 @@
 class SessionsController < ApplicationController
-  def new; end
+  before_action :redirect_signed_in_users, only: [:new]
+
+  def new;end
 
   def create
     @user = User.find_by_credentials(params[:user])
 
     if @user
       sign_in!(@user)
-      redirect_to root_url
+      redirect_back_or url_after_create
     else
       flash.now[:errors] = ["Invalid email and/or password"]
       render :new
@@ -15,6 +17,24 @@ class SessionsController < ApplicationController
 
   def destroy
     sign_out!
-    redirect_to new_session_url
+    redirect_back_or url_after_create
+  end
+
+  private
+
+  def redirect_signed_in_users
+    redirect_to url_for_signed_in_users if signed_in?
+  end
+
+  def url_after_create
+    '/'
+  end
+
+  def url_for_signed_in_users
+    url_after_create
+  end
+
+  def redirect_back_or(fallback)
+    redirect_back(fallback_location: fallback)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :redirect_signed_in_users, only: [:create, :new]
+  
   def new; end
 
   def create
@@ -6,7 +8,7 @@ class UsersController < ApplicationController
 
     if @user.save
       sign_in!(@user)
-      redirect_to root_url
+      redirect_back_or url_after_create
     else
       flash.now[:errors] = @user.errors.full_messages
       render :new
@@ -15,7 +17,19 @@ class UsersController < ApplicationController
 
   private
 
+  def redirect_signed_in_users
+    redirect_to url_for_signed_in_users if signed_in?
+  end
+
+  def url_after_create
+    '/'
+  end
+
   def user_params
     params.require(:user).permit(:email, :password)
+  end
+
+  def redirect_back_or(fallback)
+    redirect_back(fallback_location: fallback)
   end
 end
