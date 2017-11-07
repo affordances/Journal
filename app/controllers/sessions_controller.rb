3087cde866c1a807fddddb26a9219370adfb9347
Adam Strandberg <adam.strandberg@pillpack.com>
2017-11-06 23:11:19 -0500

    updated session handling to accept a GET request for guest users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,8 @@
 class SessionsController < ApplicationController
 
-  def new;end
-
-  def create
-    @user = User.find_by_credentials(params[:user])
-
-    if @user
-      sign_in!(@user)
-    end
-
+  def new
+    @user = User.new_guest
+    sign_in!(@user)
     redirect_back_or url_after_create
   end
 
